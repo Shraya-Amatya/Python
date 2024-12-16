035e2d7f883a75499b60e1564bf396c4e74d3250
Shraya Amatya <134769144+Shraya-Amatya@users.noreply.github.com>
2024-12-16 15:15:53 +0545

    First program

---
+++ First.py
@@ -0,0 +1,31 @@
+#Arithemetic operators
+
+#a=5
+#b=2
+
+#sum = a+b
+#print(sum)
+
+#assignment Operator
+
+#num = 10
+#num -= 10
+#num += 10
+
+#print ("num:", num)
+
+#logical operators
+#a= 50
+#b = 30
+#print (not False)
+#print (not (a>b))
+
+#val1= True
+#val2 = False
+##print("AND operator:",val1 and val2)
+
+#print("OR operator", val1 or val2)
+
+name=(input("Enter your Name:"))
+age=(int (input("enter your age:")))
+print("Chak",name,age)
