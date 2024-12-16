3104d969050c3b93cfc4bdafbb02a5908f0ca4e2
Shraya Amatya <134769144+Shraya-Amatya@users.noreply.github.com>
2024-12-16 15:48:23 +0545

    Practice quest

---
+++ Practice4.py
@@ -0,0 +1,9 @@
+#Wap to inpt 2 int numbers a and b Print True if a is greater than or equal to b If not print False
+num1=(int(input("Enter the first number")))
+num2=(int(input("Enter the second number")))
+#if(num1>=num2):
+#    print("True")
+#else:
+#    print("False")
+
+print(num1>=num2)
